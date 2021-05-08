e46de32296fd644aa5b0b4f3e77341ca1770ed3e
Kenneth Rothstein <skottrod@me.com>
2021-05-08 14:30:15 -0700

    updated

--- orm_app/admin.py
+++ orm_app/admin.py
@@ -1,9 +1,4 @@
 from django.contrib import admin
-
-# Register your models here.
 from .models import User
-
-@admin.register(User)
-
-class UserAdmin(admin.ModelAdmin):
-    list_display = ("first_name", "last_name", "age", "description")
+# Register your models here.
+admin.site.register(User)
